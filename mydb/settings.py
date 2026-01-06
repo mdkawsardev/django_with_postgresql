55e9996de8ee5964b9dbbc29af48f0f8371d95ec
mdkawsar <info.mdkawsardev@gmail.com>
2026-01-06 22:19:58 +0600

    django with postgres

--- mydb/settings.py
+++ mydb/settings.py
@@ -74,22 +74,22 @@ WSGI_APPLICATION = 'mydb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "books",
-        "USER": "root",
-        "PASSWORD": "",
-        "HOST": "/var/run/mysql",
-        "PORT": "3306",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "books",
+#         "USER": "root",
+#         "PASSWORD": "",
+#         "HOST": "/var/run/mysql",
+#         "PORT": "3306",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
