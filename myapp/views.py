04a453f1a9213dcd50d13faa429918b2f240758d
mdkawsar <info.mdkawsardev@gmail.com>
2026-01-07 15:36:55 +0600

    django with postgres

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from .models import Mycontact
+from .models import Mycontact, Myusers, Customers
 # Create your views here.
 def home(request):
-    context = {
-        'contexts': Mycontact.objects.all()
+    allData = {
+        'contexts1': Mycontact.objects.all(),
+        'contexts2': Myusers.objects.all(),
+        'contexts3': Customers.objects.all(),
     }
-    return render(request, 'index.html', context)
+    
+    return render(request, 'index.html', allData)
