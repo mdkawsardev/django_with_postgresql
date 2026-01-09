1c80d01a20caddac8f14ec1e3f07bfa8609a4f4d
mdkawsar <info.mdkawsardev@gmail.com>
2026-01-09 21:31:12 +0600

    django with postgres

--- myapp/views.py
+++ myapp/views.py
@@ -26,5 +26,5 @@ def insert(request):
         #     phone = phone
         # )
         # con.save()
-        msg = messages.success(request, "Data has been recorded!")
+        messages.success(request, "Data has been recorded!")
     return render(request, 'insert.html')
